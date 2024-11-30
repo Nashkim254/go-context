ca4efd2c8ae92d31a99826504a91a94deadcbd6d
programming6131 <kimutainashon@gmail.com>
2024-11-30 13:52:10 +0300

    chans with select

--- main.go
+++ main.go
@@ -8,21 +8,41 @@ import (
 )
 
 func main() {
+	start := time.Now()
 	ctx := context.Background()
 	val, err := fetchUser(ctx, 2)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(val)
-	fmt.Println("Took:", val)
+	fmt.Println("Took:", time.Since(start))
+}
+
+type Response struct {
+	value int
+	err   error
 }
 
 func fetchUser(ctx context.Context, userId int) (int, error) {
-	val, err := callThirdPartyDependency()
-	if err != nil {
-		return 0, err
+	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*200)
+	defer cancel()
+	respChan := make(chan Response)
+
+	go func() {
+		val, err := callThirdPartyDependency()
+		respChan <- Response{
+			value: val,
+			err:   err,
+		}
+	}()
+	for {
+		select {
+		case <-ctx.Done():
+			return 0, fmt.Errorf("fetching data took too long")
+		case resp := <-respChan:
+			return resp.value, resp.err
+		}
 	}
-	return val, err
 }
 
 func callThirdPartyDependency() (int, error) {
